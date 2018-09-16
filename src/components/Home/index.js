0b67b26971b20d0c287aa7b4cbfa8d92855d7fa8
David Kopal <kopaldvd@gmail.com>
2018-09-16 21:34:41 +0200

    reusable emotion components created

---
+++ src/components/Home/index.js
@@ -0,0 +1,103 @@
+import styled from "react-emotion"
+import profilePhoto from "../../assets/images/profile.png"
+
+export const ListItem = styled.li`
+  cursor: default;
+  margin-bottom: 16px;
+`
+
+export const A = styled.span`
+  box-shadow: inset 0 -1px 0 #333;
+    color: #333;
+    -webkit-transition: all .2s ease-in-out;
+    transition: all .2s ease-in-out;
+  &:hover {
+    box-shadow: inset 0 -1px 0 #F85649
+    color: #F85649;
+  }
+`
+
+export const Ol = styled.ol`
+  list-style: decimal-leading-zero inside;
+  margin: 0;
+`
+
+export const Container = styled.div`
+  margin: 0 auto;
+
+  @media (min-width: 1024px) and (min-height: 680px) {
+    align-items: center;
+    display: flex;
+    height: 100vh;
+    width: 100%;
+  }
+`
+
+export const Title = styled.h1`
+  font-size: 4rem;
+  margin: 0 0 16px;
+  font-weight: 900;
+  line-height: 1;
+  text-align: center;
+`
+
+export const Subtitle = styled.h2`
+  font-family: "Cousine", monospace;
+  font-size: 1.22222rem;
+  font-weight: 400;
+  margin: 0 0 20px;
+  text-align: center;
+
+  @media (min-width: 768px) {
+    font-size: 1.44444rem;
+  }
+`
+
+export const Name = styled.h2`
+  font-size: 2rem;
+  text-align: center;
+`
+
+export const Image = styled.div`
+  background-image: url(${profilePhoto});
+  height: 180px;
+  width: 180px;
+  background-size: cover;
+  border-radius: 50%;
+  @media (min-width: $ds-breakpoint-iphone6-width) {
+    height: 120px;
+    padding-top: 6px;
+    width: 120px;
+  }
+
+  @media (min-width: $ds-breakpoint-tablet) {
+    height: 140px;
+    padding-top: 20px;
+    width: 140px;
+  }
+`
+
+export const Description = styled.span`
+  margin-bottom: 10px;
+`
+
+export const Col1 = styled.div`
+  flex: 70%;
+`
+
+export const Col2 = styled.div`
+  flex: 30%;
+`
+
+export const Break = styled.hr`
+  border: 0;
+  margin: 0 0 1.11111rem;
+  max-width: 100%;
+  height: 20px;
+  overflow: hidden;
+  position: relative;
+
+  @media (min-width: 768px) {
+    margin: 1.11111rem 0 1.22222rem;
+  }
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,232 @@
 import React, { Component } from "react"
 import { Link, graphql } from "gatsby"
+import { Box, Flex } from "../components/Layout"
 import ClockIcon from "react-icons/lib/fa/clock-o"
-import styled from 'react-emotion';
-
 import Layout from "../layouts"
 import PostIcons from "../components/PostIcons"
-
 import { rhythm } from "../utils/typography"
+import {
+  Title,
+  Subtitle,
+  Name,
+  Col1,
+  Col2,
+  Image,
+  Break,
+  Description,
+  A,
+  ListItem,
+  Ol,
+} from "../components/Home"
 
-const Title = styled.h1`
-  font-size: 4rem;
-  margin: 0 0 16px;
-  font-weight: 900;
-  line-height: 1;
-  text-align: center;
-`;
+const messages = {
+  title: "David Kopal",
+  aka: "A.K.A.",
+  subtitle: "Coding lawyer",
+  invitation: "Hi, welcome to my blog!",
+  desc1: "Lawyer turned software engineer living in Prague. I make stuff at",
+  desc2:
+    "I’m passionate about sharing my experiences with the community via my blog and as a speaker at conferences and meetups. If I’m not giving a talk or learning new technologies, I keep myself busy by organizing ReasonML meetups in Prague, swinging kettlebells, and cooking.",
+}
+
+const Comp = () => (
+  <Box width={720} m="0 auto">
+    <Flex>
+      <Col1>
+        <Title>{messages.title}</Title>
+        <Subtitle>{messages.aka}</Subtitle>
+        <Name>{messages.subtitle}</Name>
+        <Subtitle>{messages.invitation}</Subtitle>
+      </Col1>
+      <Col2>
+        <Image />
+      </Col2>
+    </Flex>
+    <Break />
+    <Description>{messages.desc1}</Description>
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      href="https://www.blueberry.io/"
+    >
+      {" "}
+      <A>Blueberry</A>
+      <span role="img" aria-labelledby="book">
+        {"✨. "}
+      </span>
+    </a>
+    <Description>{messages.desc2}</Description>
+    <Break />
+    <div id="menu" className="slideout-menu" role="navigation">
+      <div className="navigation">
+        <Ol>
+          <ListItem>
+            <Link to="/blog">
+              <A>Blog</A>
+              <span role="img" aria-labelledby="book">
+                {` 📖`}
+              </span>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link to="/blog">
+              <A>Talks</A>
+              <span role="img" aria-labelledby="speaking">
+                {` 🗣`}
+              </span>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <span>Get in touch with me via </span>
+            <span role="img" aria-labelledby="speaking">
+              {` 📬 `}
+            </span>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="mailto:kopaldvd@gmail.com"
+            >
+              <A>Email</A>
+            </a>
+            {`, `}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://twitter.com/coding_lawyer"
+            >
+              <A>Twitter</A>
+            </a>
+            {`, `}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/codinglawyer"
+            >
+              <A>Github</A>
+            </a>
+            {`, `}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://medium.com/@codinglawyer"
+            >
+              <A>Medium</A>
+            </a>
+            {`, `}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.linkedin.com/in/david-kopal-54219199/"
+            >
+              <A>LinkedIn</A>
+            </a>
+          </ListItem>
+        </Ol>
+      </div>
+    </div>
+  </Box>
+)
 
 class Home extends Component {
   render() {
     const data = this.props.data
-
     return (
       <Layout>
         <div css={{ marginBottom: rhythm(1) }}>
-          <h1>Pages</h1>
+          <Flex>
+            <Col1>
+              <Title>{messages.title}</Title>
+              <Subtitle>{messages.aka}</Subtitle>
+              <Name>{messages.subtitle}</Name>
+              <Subtitle>{messages.invitation}</Subtitle>
+            </Col1>
+            <Col2>
+              <Image />
+            </Col2>
+          </Flex>
+          <Break />
+          <Description>{messages.desc1}</Description>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.blueberry.io/"
+          >
+            {" "}
+            <A>Blueberry</A>
+            <span role="img" aria-labelledby="book">
+              {"✨. "}
+            </span>
+          </a>
+          <Description>{messages.desc2}</Description>
+          <Break />
+          <div id="menu" className="slideout-menu" role="navigation">
+            <div className="navigation">
+              <Ol>
+                <ListItem>
+                  <Link to="/blog">
+                    <A>Blog</A>
+                    <span role="img" aria-labelledby="book">
+                      {` 📖`}
+                    </span>
+                  </Link>
+                </ListItem>
+                <ListItem>
+                  <Link to="/blog">
+                    <A>Talks</A>
+                    <span role="img" aria-labelledby="speaking">
+                      {` 🗣`}
+                    </span>
+                  </Link>
+                </ListItem>
+                <ListItem>
+                  <span>Get in touch with me via </span>
+                  <span role="img" aria-labelledby="speaking">
+                    {` 📬 `}
+                  </span>
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="mailto:kopaldvd@gmail.com"
+                  >
+                    <A>Email</A>
+                  </a>
+                  {`, `}
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://twitter.com/coding_lawyer"
+                  >
+                    <A>Twitter</A>
+                  </a>
+                  {`, `}
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://github.com/codinglawyer"
+                  >
+                    <A>Github</A>
+                  </a>
+                  {`, `}
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://medium.com/@codinglawyer"
+                  >
+                    <A>Medium</A>
+                  </a>
+                  {`, `}
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://www.linkedin.com/in/david-kopal-54219199/"
+                  >
+                    <A>LinkedIn</A>
+                  </a>
+                </ListItem>
+              </Ol>
+            </div>
+          </div>
+          {/* <h1>Pages</h1>
           {data.allWordpressPage.edges.map(({ node }) => (
             <div key={node.slug}>
               <Link to={node.slug} css={{ textDecoration: `none` }}>
@@ -51,7 +254,8 @@ class Home extends Component {
             <div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
             <PostIcons node={node} />
           </div>
-        ))}
+        ))} */}
+        </div>
       </Layout>
     )
   }
