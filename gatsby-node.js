8cb6132b4b85f4c4f98107a9121e894fa4d5fed6
David Kopal <kopaldvd@gmail.com>
2018-10-05 15:24:25 +0200

    pages data are not fetched from wordpress site

--- gatsby-node.js
+++ gatsby-node.js
@@ -17,17 +17,18 @@ exports.createPages = ({ graphql, actions }) => {
     // it like the site has a built-in database constructed
     // from the fetched data that you can run queries against.
 
-    // ==== PAGES (WORDPRESS NATIVE) ====
+    // ==== POSTS (WORDPRESS NATIVE AND ACF) ====
     graphql(
       `
         {
-          allWordpressPage {
+          allWordpressPost {
             edges {
               node {
                 id
                 slug
                 status
                 template
+                format
               }
             }
           }
@@ -36,69 +37,19 @@ exports.createPages = ({ graphql, actions }) => {
     )
       .then(result => {
         if (result.errors) {
-          console.log(result.errors)
           reject(result.errors)
         }
-
-        // Create Page pages.
-        const pageTemplate = path.resolve(`./src/templates/page.js`)
+        const postTemplate = path.resolve(`./src/templates/post.js`)
         // We want to create a detailed page for each
-        // page node. We'll just use the Wordpress Slug for the slug.
-        // The Page ID is prefixed with 'PAGE_'
-        _.each(result.data.allWordpressPage.edges, edge => {
-          // Gatsby uses Redux to manage its internal state.
-          // Plugins and sites can use functions like "createPage"
-          // to interact with Gatsby.
+        // post node. We'll just use the Wordpress Slug for the slug.
+        // The Post ID is prefixed with 'POST_'
+        _.each(result.data.allWordpressPost.edges, edge => {
           createPage({
-            // Each page is required to have a `path` as well
-            // as a template component. The `context` is
-            // optional but is often necessary so the template
-            // can query data specific to each page.
-            path: `/${edge.node.slug}/`,
-            component: slash(pageTemplate),
+            path: edge.node.slug,
+            component: slash(postTemplate),
             context: {
               id: edge.node.id,
             },
-          })
-        })
-      })
-      // ==== END PAGES ====
-      // ==== POSTS (WORDPRESS NATIVE AND ACF) ====
-      .then(() => {
-        graphql(
-          `
-            {
-              allWordpressPost {
-                edges {
-                  node {
-                    id
-                    slug
-                    status
-                    template
-                    format
-                  }
-                }
-              }
-            }
-          `,
-        ).then(result => {
-          if (result.errors) {
-            console.log(result.errors)
-            reject(result.errors)
-          }
-          const postTemplate = path.resolve(`./src/templates/post.js`)
-          console.log('RESUlt', result)
-          // We want to create a detailed page for each
-          // post node. We'll just use the Wordpress Slug for the slug.
-          // The Post ID is prefixed with 'POST_'
-          _.each(result.data.allWordpressPost.edges, edge => {
-            createPage({
-              path: edge.node.slug,
-              component: slash(postTemplate),
-              context: {
-                id: edge.node.id,
-              },
-            })
           })
         })
       })
@@ -124,7 +75,6 @@ exports.createPages = ({ graphql, actions }) => {
             reject(result.errors)
           }
           const tagTemplate = path.resolve(`./src/templates/tag.js`)
-          console.log('RESULT', result)
           _.each(result.data.allWordpressTag.edges, edge => {
             createPage({
               path: edge.node.slug,
