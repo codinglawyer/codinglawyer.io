4738a7cd04d84423359da784752276c659e121bc
David Kopal <kopaldvd@gmail.com>
2018-10-07 19:56:11 +0200

    GA added

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,13 @@ module.exports = {
         useACF: true,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-96755667-2',
+        head: true,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-emotion`,
