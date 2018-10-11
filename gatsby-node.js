582caca54b6f2545950e47915e796f4a7dfd56ae
David Kopal <kopaldvd@gmail.com>
2018-10-11 17:48:28 +0200

    comments deleted

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,21 +3,9 @@ const Promise = require(`bluebird`)
 const path = require(`path`)
 const slash = require(`slash`)
 
-// Implement the Gatsby API “createPages”. This is
-// called after the Gatsby bootstrap is finished so you have
-// access to any information necessary to programmatically
-// create pages.
-// Will create pages for Wordpress pages (route : /{slug})
-// Will create pages for Wordpress posts (route : /post/{slug})
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
   return new Promise((resolve, reject) => {
-    // The “graphql” function allows us to run arbitrary
-    // queries against the local Wordpress graphql schema. Think of
-    // it like the site has a built-in database constructed
-    // from the fetched data that you can run queries against.
-
-    // ==== POSTS (WORDPRESS NATIVE AND ACF) ====
     graphql(
       `
         {
@@ -40,9 +28,6 @@ exports.createPages = ({ graphql, actions }) => {
           reject(result.errors)
         }
         const postTemplate = path.resolve(`./src/templates/post.js`)
-        // We want to create a detailed page for each
-        // post node. We'll just use the Wordpress Slug for the slug.
-        // The Post ID is prefixed with 'POST_'
         _.each(result.data.allWordpressPost.edges, edge => {
           createPage({
             path: `posts/${edge.node.slug}`,
@@ -53,7 +38,6 @@ exports.createPages = ({ graphql, actions }) => {
           })
         })
       })
-      // ==== END POSTS ====
       .then(() => {
         graphql(
           `
