ce2e40d93ec960ad69e6a6cbe5742e803d245523
David Kopal <kopaldvd@gmail.com>
2018-12-18 21:34:06 +0100

    tags extracted from markdown posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,21 +1,31 @@
 const path = require(`path`)
 const slash = require(`slash`)
-const tags = require('./src/data/tags_node')
+
+const getAllTags = allPosts =>
+  allPosts.data.allMarkdownRemark.edges.reduce(
+    (acc, val) => [
+      ...acc,
+      ...val.node.frontmatter.tags.map(tag => ({
+        name: tag,
+        count: 0,
+      })),
+    ],
+    [],
+  )
+
+const removeDuplicates = tags =>
+  tags.reduce((acc, val) => {
+    const tagExists = acc.find(tag => tag.name === val.name)
+    if (tagExists) {
+      return acc.map(
+        tag => (tag.name === val.name ? { ...tag, count: tag.count + 1 } : tag),
+      )
+    }
+    return [...acc, { name: val.name, count: 0 }]
+  }, [])
 
 exports.createPages = ({ actions, graphql }) => {
   const { createPage } = actions
-  const tgs = tags.tags()
-
-  const tagTemplate = path.resolve(`./src/templates/tag.js`)
-  tgs.forEach(tag => {
-    createPage({
-      path: `tags/${tag.slug}`,
-      component: slash(tagTemplate),
-      context: {
-        name: tag.name,
-      },
-    })
-  })
 
   const postTemplate = path.resolve(`src/templates/post.js`)
   return graphql(`
@@ -30,6 +40,7 @@ exports.createPages = ({ actions, graphql }) => {
               slug
               description
               tags
+              thumbnail
             }
           }
         }
@@ -39,12 +50,37 @@ exports.createPages = ({ actions, graphql }) => {
     if (result.errors) {
       return Promise.reject(result.errors)
     }
+
+    const allTags = removeDuplicates(getAllTags(result))
+    const posts = path.resolve(`./src/templates/posts.js`)
+    createPage({
+      path: '/posts',
+      component: slash(posts),
+      context: {
+        allTags,
+      },
+    })
+
+    const tagTemplate = path.resolve(`./src/templates/tag.js`)
+    allTags.forEach(tag => {
+      createPage({
+        path: `tags/${tag.name}`,
+        component: slash(tagTemplate),
+        context: {
+          name: tag.name,
+        },
+      })
+    })
+
     result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      const thumbnail = node.frontmatter.thumbnail[0]
+
       createPage({
         path: `posts/${node.frontmatter.slug}`,
         component: postTemplate,
         context: {
           slug: node.frontmatter.slug,
+          thumbnailRegex: `/${thumbnail}/`,
         },
       })
     })
